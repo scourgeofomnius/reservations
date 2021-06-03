fe9517723abd452ad6290ee6003f481c4a4b96a1
scourgeofomnius <joseph.a.salmans@gmail.com>
2021-06-03 14:48:05 -0700

    first commit

---
+++ src/main/java/com/airline/reservations/Reservation.java
@@ -0,0 +1,4 @@
+package com.airline.reservations;
+
+public class Reservation {
+}
---
+++ src/test/java/com/airline/reservations/ReserveappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airline.reservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
